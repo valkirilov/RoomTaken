8375dc716934a3724c4fe37a3a1d92cfe5c14019
Valentin Kirilov <val.kirilov@gmail.com>
2014-04-26 15:57:50 +0300

    Deleted more info.

--- SPA/app/libs/ng-Fx/src/animate.js
+++
@@ -1,7 +0,0 @@
-// Collect all the animations into one master module. this module is the main module
-
-(function(angular){
-  "use strict";
-  angular.module('fx.animations',['fx.animations.fades','fx.animations.bounces']);
-}(angular));
-
