21a42e1754de60446b167e18c923830661ab203f
Valentin Kirilov <val.kirilov@gmail.com>
2014-04-26 15:01:09 +0300

    Animations added but not working.

--- SPA/app/js/controllers.js
+++ SPA/app/js/controllers.js
@@ -15,21 +15,15 @@ angular.module('roomTaken.controllers', [])
     
     $scope.snippets = null;
     
-    $scope.shedule = null;
+    $scope.schedule = null;
     
     $scope.init = function() {
         
         console.log(ResourceService);
         
-        ResourceService.getSnippets().then(function(success) {
+        ResourceService.getSchedule().then(function(success) {
             console.log(success);
-        }, function(error) {
-            console.log(error);
-        });
-        
-        ResourceService.getShedule().then(function(success) {
-            console.log(success);
-            $scope.shedule = success.data.shedule;
+            $scope.shedule = success.data;
         }, function(error) {
             console.log(error);
         });
